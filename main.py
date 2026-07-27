45eff1d6fb9edbadbd07f662f39b479debe7ccff
TeddiZen <2576903455@qq.com>
2026-07-27 22:47:00 +0800

    修变量

--- main.py
+++ main.py
@@ -64,9 +64,9 @@ class MyPlugin(Star):
             uid = raw["user_id"]
             
             logger.info(f"新成员 {uid} 进入群 {group_id}")
-            origin = f"onebot:group_{target_gid}"
+            origin = f"onebot:group_{group_id}"
             chain = [
-                Comp.At(qq=join_qq),
+                Comp.At(qq=uid),
                 Comp.Plain(f" 欢迎新成员 {uid} 进入群 {group_id}")
             ]
             await self.context.send_message(origin, chain)
